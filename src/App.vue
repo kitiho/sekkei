00f0b0f839ce9d2f282ee77f8f096361b98631b5
enoshima7 <duanjipeng0328@gmail.com>
2022-04-03 01:00:10 +0800

    feat: homepage

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
+  <main w-full h-full>
     <router-view />
     <Footer />
   </main>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,18 @@
 
 </script>
 <template>
-  <div>1</div>
+  <div w-full h-full p-40>
+    <div w-full text="left" font-mono mb-2 mb-4 text-2xl font-bold>
+      100 days
+    </div>
+    <div w-full flex="~" flex-wrap text-xl>
+      <a v-for="item in 60" :key="item" block w-60 mr-4 font-400 class="hover:op100" cursor-pointer>
+        <span mr-2 opacity-50 font-mono>001</span>
+        <b text="gray-500/80">Mass</b>
+      </a>
+    </div>
+  </div>
 </template>
-<style scoped>
+<style  scoped>
+
 </style>
